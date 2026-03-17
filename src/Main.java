d45dadd78c0a43473f1ca6512016c2ab926ecdc8
Oleh Medvid <s33408@pjwstk.edu.pl>
2026-03-17 09:03:54 +0100

    Book class and the first test objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Book book1 = new Book("book", "Author", 100, true);
+        book1.printInfo();
+        System.out.println("------------");
+        book1.borrow();
+        System.out.println("------------");
+        book1.printInfo();
     }
 }
